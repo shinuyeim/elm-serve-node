fc2fa973b221f7e1de9c127c6b4340b8c2ece002
pennbay <pennbaymail@gmail.com>
2020-04-19 18:51:42 +0800

    fix populatedb warning

--- populatedb/populatedb.js
+++ populatedb/populatedb.js
@@ -3,59 +3,70 @@
 // Get arguments passed on command line
 var userArgs = process.argv.slice(2);
 
-if (!userArgs[0].startsWith('mongodb')) {
-    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-    return
+if (!userArgs[0].startsWith("mongodb")) {
+    console.log(
+        "ERROR: You need to specify a valid mongodb URL as the first argument"
+    );
+    return;
 }
 
-var async = require('async');
+var async = require("async");
 var Admin = require("../models/admin/admin.js");
 var Merchant = require("../models/merchant/merchant.js");
 
-const adminData = require('./data/adminData');
-const merchantData = require('./data/merchantData');
+const adminData = require("./data/adminData");
+const merchantData = require("./data/merchantData");
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var mongoDB = userArgs[0];
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-async.series([
-    createAdmin,
-    createMerchant,
-],
+async.series(
+    [createAdmin, createMerchant],
     // Optional callback
     function (err, results) {
         if (err) {
-            console.log('FINAL ERR: ' + err);
+            console.log("FINAL ERR: " + err);
         }
         // All done, disconnect from database
         mongoose.connection.close();
-    });
-
+    }
+);
 
 function adminCreate(user_name, password, register_date, city, privilege, cb) {
-    admindetail = { user_name, password, register_date, city, privilege }
+    admindetail = { user_name, password, register_date, city, privilege };
 
     var admin = new Admin(admindetail);
 
     admin.save(function (err) {
         if (err) {
-            cb(err, null)
-            return
+            cb(err, null);
+            return;
         }
-        console.log('New Admin: ' + admin);
-        cb(null, admin)
+        console.log("New Admin: " + admin);
+        cb(null, admin);
     });
 }
 
 function createAdmin(cb) {
-
-    async.eachLimit(adminData, 10, function (item, callback) {
-        adminCreate(item.name, item.password, item.date, item.address, item.privilege, callback);
-    },cb);
+    async.eachLimit(
+        adminData,
+        10,
+        function (item, callback) {
+            adminCreate(
+                item.name,
+                item.password,
+                item.date,
+                item.address,
+                item.privilege,
+                callback
+            );
+        },
+        cb
+    );
 
     // async.series([
     //     // function (callback) {
@@ -69,24 +80,48 @@ function createAdmin(cb) {
     //     cb);
 }
 
-function merchantCreate(shop_name,register_date, address, phone,introduction, cb) {
-    const merchantdetail = { shop_name, register_date, address, phone,introduction }
+function merchantCreate(
+    shop_name,
+    register_date,
+    address,
+    phone,
+    introduction,
+    cb
+) {
+    const merchantdetail = {
+        shop_name,
+        register_date,
+        address,
+        phone,
+        introduction,
+    };
 
     var merchant = new Merchant(merchantdetail);
 
     merchant.save(function (err) {
         if (err) {
-            cb(err, null)
-            return
+            cb(err, null);
+            return;
         }
-        console.log('New Merchant: ' + merchant);
-        cb(null, merchant)
+        console.log("New Merchant: " + merchant);
+        cb(null, merchant);
     });
 }
 
 function createMerchant(cb) {
-
-    async.eachLimit(merchantData, 10, function (item, callback) {
-        merchantCreate(item.name, item.date, item.address,item.phone, item.introduction, callback);
-    },cb);
+    async.eachLimit(
+        merchantData,
+        10,
+        function (item, callback) {
+            merchantCreate(
+                item.name,
+                item.date,
+                item.address,
+                item.phone,
+                item.introduction,
+                callback
+            );
+        },
+        cb
+    );
 }
